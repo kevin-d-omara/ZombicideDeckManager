4adcd47c609f2be56a785d4494224869786d710e
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-05-06 20:40:05 -0700

    Add method to transform Icon to Image

--- Assets/Scripts/Icon.cs
+++ Assets/Scripts/Icon.cs
@@ -7,6 +7,9 @@ namespace ZombicideDeckManager
 {
     /// <summary>
     /// Metadata for a sprite.
+    /// 
+    /// Note: Sprites used as an Image (UI) will automatically have 'width' and 'height' set to
+    /// native values via Image.SetNativeSize.
     /// </summary>
     [Serializable]
     public class Icon
@@ -17,5 +20,25 @@ namespace ZombicideDeckManager
         public float rotation = 0.0f;
         public float scale = 1.0f;
         public Color color = Color.white;
+
+        /// <summary>
+        /// Set the Image with this Sprite and metadata.
+        /// </summary>
+        public void SetImage(Image image)
+        {
+            image.sprite = sprite;
+            image.SetNativeSize();
+            image.color = color;
+
+            var rect = image.rectTransform;
+            rect.localEulerAngles = new Vector3(0f, 0f, rotation);
+            rect.localScale = new Vector3(scale, scale, scale);
+
+            // TODO: Make offsets not stack (i.e. if A.SetImage(B) is called twice, B's positions should be the same both times).
+            var x = rect.localPosition.x + offset.x;
+            var y = rect.localPosition.y + offset.y;
+            var z = rect.localPosition.z;
+            rect.localPosition = new Vector3(x, y, z);
+        }
     }
 }
