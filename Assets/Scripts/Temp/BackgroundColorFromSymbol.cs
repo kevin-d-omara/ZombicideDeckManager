dc7de41c0c20c5cd360b7c73bce5293401d86de2
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-06-10 20:30:49 -0700

    Add color gradient

---
+++ Assets/Scripts/Temp/BackgroundColorFromSymbol.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ZombicideDeckManager
+{
+    public class BackgroundColorFromSymbol : MonoBehaviour
+    {
+        public Image symbol;
+        public Image background;
+
+        private void Awake()
+        {
+            float h, s, v;
+            Color.RGBToHSV(symbol.color, out h, out s, out v);
+
+            background.color = Color.HSVToRGB(h, s, 1);
+        }
+    }
+}
